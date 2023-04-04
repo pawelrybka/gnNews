5e4bf5f786147d2f1007f7219766909be069fb00
Pawel Rybka <rrybka1999@gmail.com>
2023-04-05 00:01:14 +0200

    Basic structure

---
+++ src/Components/Counter.tsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import styles from './Counter.module.css'
+
+function Counter() {
+  
+  const [count, setCount] = useState(0)
+
+  return (
+  <div className={styles.counter}>
+    <h1>Vite + React</h1>
+    <button onClick={() => setCount((count) => count + 1)}>
+      Count is {count}
+    </button>
+  </div>
+  )
+}
+
+export default Counter
+
