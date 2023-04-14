3ff5d0815e57201f292976f6a4859eaca37942d7
Pawel Rybka <rrybka1999@gmail.com>
2023-04-14 13:34:52 +0200

    Add informationModal

---
+++ src/Components/InformationModal/InformationModal.tsx
@@ -0,0 +1,42 @@
+import { AiOutlineClose } from 'react-icons/ai';
+import { Link } from 'react-router-dom';
+import styles from './InformationModal.module.css'
+
+interface NewsArticle {
+  title: string
+  country: string
+  description: string
+  link: string
+}
+
+interface Props {
+  informationModalVisible: boolean
+  setInformationModalVisible: React.Dispatch<React.SetStateAction<boolean>>
+  selectedArticle: NewsArticle | null;
+}
+
+function InformationModal({ informationModalVisible, setInformationModalVisible, selectedArticle }: Props) {
+  
+  return (
+    <div className={styles.informationmodal}>
+      <div className={styles.informationmodal__header}>
+        <h3>{selectedArticle?.title}</h3>
+        <button onClick={() => setInformationModalVisible(!informationModalVisible)}>
+          <AiOutlineClose size={20}/>
+        </button>
+      </div>
+      <div className={styles.informationmodal__content}>
+        <p>{selectedArticle?.description}</p>
+      </div>
+      <div className={styles.informationmodal__footer}>
+        <button onClick={() => setInformationModalVisible(!informationModalVisible)}>Return</button>
+        <Link to={selectedArticle?.link ? selectedArticle.link : ''}>
+          <button>Go to the article!</button>
+        </Link>
+      </div>
+    </div>
+  )
+}
+
+export default InformationModal
+
--- src/Components/Main/MainContent/DisplayState/DisplayState.tsx
+++ src/Components/Main/MainContent/DisplayState/DisplayState.tsx
@@ -2,23 +2,31 @@ import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import { RootState } from '../../../../Redux/displaySlice';
+import InformationModal from '../../../InformationModal/InformationModal';
 import LoadingState from '../LoadingState/LoadingState';
 import styles from './DisplayState.module.css';
 
 interface NewsArticle {
-  title: string;
-  country: string;
+  title: string
+  country: string
+  description: string
+  link: string
 }
+
 
 function DisplayContent() {
 
+  const country = useParams();
+  
   const displayColumn = useSelector((state: RootState) => state.display.displayColumn);
 
+  const [isLoading, setIsLoading] = useState(false);
+  
   const [newsData, setNewsData] = useState<NewsArticle[]>([]);
   
-  const [isLoading, setIsLoading] = useState(false);
+  const [informationModalVisible, setInformationModalVisible] = useState(false)
 
-  const country = useParams();
+  const [selectedArticle, setSelectedArticle] = useState<NewsArticle | null>(null);
 
   async function fetchData() {
     setIsLoading(true);
@@ -34,16 +42,27 @@ function DisplayContent() {
 
   return (
     <div className={`${styles.displaycontent} ${displayColumn ? styles.rows : ''}`}>
-      {isLoading ? (
+      {informationModalVisible && 
+        <InformationModal
+          informationModalVisible={informationModalVisible} 
+          setInformationModalVisible={setInformationModalVisible}
+          selectedArticle={selectedArticle}
+        />
+      }
+      
+      {isLoading ? 
         <LoadingState />
-      ) : (
+       : 
         newsData.map((article, index) => (
-          <div key={index} className={styles.article}>
+          <div key={index} className={styles.article} onClick={() => {
+            setSelectedArticle(article)
+            setInformationModalVisible(!informationModalVisible)
+          }}>
             <h3>{article.title}</h3>
             <p>{article.country}</p>
           </div>
         ))
-      )}
+      }
     </div>
   );
 }
